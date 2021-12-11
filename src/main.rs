51284905bbdf5821928bd25ec334b52d67b293b9
KernelErr0r <villainkernelerror@gmail.com>
2021-12-11 13:53:09 +0100

    Add basic CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::{App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("bsp-tool")
+        .author("Bakamono")
+        .version("0.1")
+        .subcommand(
+            SubCommand::with_name("info").about("Provides information about a BSP file's content"),
+        )
+        .subcommand(
+            SubCommand::with_name("convert")
+                .about("Converts a BSP file into another format and vice versa"),
+        )
+        .subcommand(SubCommand::with_name("extract").about("Extracts a BSP file"))
+        .subcommand(SubCommand::with_name("verify").about("Verifies if a BSP file isn't corrupted"))
+        .get_matches();
 }
