97b6546ae134b48e8eed1176f369298891946179
KernelErr0r <villainkernelerror@gmail.com>
2021-12-11 19:16:44 +0100

    Implement simple verify command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use clap::{App, SubCommand};
+use std::fs::File;
+
+use clap::{App, Arg, SubCommand};
+use spooky_bsp::BspDecoder;
 
 fn main() {
     let matches = App::new("bsp-tool")
@@ -12,6 +15,27 @@ fn main() {
                 .about("Converts a BSP file into another format and vice versa"),
         )
         .subcommand(SubCommand::with_name("extract").about("Extracts a BSP file"))
-        .subcommand(SubCommand::with_name("verify").about("Verifies if a BSP file isn't corrupted"))
+        .subcommand(
+            SubCommand::with_name("verify")
+                .about("Verifies if a BSP file isn't corrupted")
+                .arg(
+                    Arg::with_name("input")
+                        .short("i")
+                        .required(true)
+                        .takes_value(true),
+                ),
+        )
         .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("verify") {
+        let input = matches.value_of("input").unwrap();
+
+        match File::open(input) {
+            Ok(file) => match BspDecoder::decode(file) {
+                Ok(_) => println!("File is not corrupted"),
+                Err(_) => println!("File is corrupted"),
+            },
+            Err(_) => println!("Failed to open a file"),
+        }
+    }
 }
